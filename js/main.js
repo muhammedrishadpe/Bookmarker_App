d3059d56a95ba68a1bce0c3d087cda934f392700
muhammedrishadpe <rishadpe@gmail.com>
2021-08-01 15:38:38 +0530

    save bookmark & Listen for form submit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert(1);
+// Lisen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e){
+    console.log('its works');
+    // Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+    
+    var Bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+    console.log(Bookmark);
+
+    // Prevent form  from submitting
+    e.preventDefault();
+}
